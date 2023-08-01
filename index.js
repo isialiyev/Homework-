be89ed8e7d3bc539074c9d665a829d64ee24d95d
isialiyev <israfil.i.aliyev@gmail.com>
2023-08-02 03:08:41 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// let a = 5
+// let b = 7
+// let z = a
+// a=z+2
+// let y = b
+// b=y-2
+// console.log(a, b)
+
+// let a = 5
+// let b = 7 
+// a = 5+2
+// b = 7-2
+// console.log(a, b)
+
+// let singleSide = 3
+// let Perimeter = singleSide*4
+// console.log(Perimeter)
+
+// let singleSide = 3
+// let Area = singleSide**2
+// console.log(Area)
+
+// let length = 6
+// let width =3
+// let Perimeter = length*2+width*2
+// console.log(Perimeter)
+
+// let lenght = 6
+// let width = 3
+// let Area = lenght*width
+// console.log(Area)
+
+// let login = "Israfil"
+// let password = 1234
+// login === "Israfil" && password === 1234 ? console.log("correct"):console.log("incorrect")
+
